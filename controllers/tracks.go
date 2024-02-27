3697f245eb281bc83bae61b6ccdb22bc5c053521
Fedor aka MXkodo <nemchinow2018@mail.ru>
2024-02-27 16:17:38 +0400

    add database.go

--- controllers/tracks.go
+++ controllers/tracks.go
@@ -15,20 +15,26 @@ type CreateItemInput struct {
  	Budget    bool   `json:"budget"  binding:"required"`
  	Desc      string `json:"desc"`
 }
+type UpdateItemInput struct {
+   InvNumber string `json:"invnumber"`
+ 	Name      string `json:"name"`
+ 	Storage   string `json:"storage"`
+	Date      string `json:"date"`
+ 	Budget    bool   `json:"budget"`
+ 	Desc      string `json:"desc"`
+}
 
 // GET /items
 // Получаем список всех позиций
 func GetAllItems(context *gin.Context) {
-   db := models.ConnectDB()
    var items []models.Item
-   db.Find(&items)
+   models.DB.Find(&items)
    context.JSON(http.StatusOK, gin.H{"items": items})
 }
 
 // POST /items
 // Создание позиции
 func CreateItem(context *gin.Context) {
-   db := models.ConnectDB()
    var input CreateItemInput
    if err := context.ShouldBindJSON(&input); err != nil {
       context.JSON(http.StatusBadRequest, gin.H{"error":      err.Error()})
@@ -43,7 +49,50 @@ func CreateItem(context *gin.Context) {
 	Budget: input.Budget, 
 	Desc: input.Desc,}
 
-   db.Create(&item)
+   models.DB.Create(&item)
+
+   context.JSON(http.StatusOK, gin.H{"items": item})
+}
+
+// GET /items/:id
+// Получение одной позиции по ID
+func GetItem(context *gin.Context) {
+   // Проверяем имеется ли запись
+   var item models.Item
+   if err := models.DB.Where("id = ?", context.Param("id")).First(&item).Error; err != nil {
+      context.JSON(http.StatusBadRequest, gin.H{"error": "Запись не существует"})
+      return
+   }
+
+   context.JSON(http.StatusOK, gin.H{"tracks": item})
+}
+
+// PATCH /items/:id
+// Изменения информации
+func UpdateItem(context *gin.Context) {
+   // Проверяем имеется ли такая запись перед тем как её менять
+   var item models.Item
+   if err := models.DB.Where("id = ?", context.Param("id")).First(&item).Error; err != nil {
+      context.JSON(http.StatusBadRequest, gin.H{"error": "Запись не существует"})
+      return
+   }
+
+   var input UpdateItemInput
+   if err := context.ShouldBindJSON(&input); err != nil {
+      context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+      return
+   }
+
+	updateFields := map[string]interface{}{
+		"InvNumber": input.InvNumber,
+		"Name":      input.Name,
+		"Storage":   input.Storage,
+		"Date":      input.Date,
+		"Budget":    input.Budget,
+		"Desc":      input.Desc,
+	}
+
+   models.DB.Model(&item).Updates(updateFields)
 
    context.JSON(http.StatusOK, gin.H{"items": item})
 }
--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"github.com/MXkodo/inventory/controllers"
-	"github.com/MXkodo/inventory/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	route := gin.Default()
-	models.ConnectDB()
 	route.GET("/items", controllers.GetAllItems)
 	route.POST("/items", controllers.CreateItem)
+	route.GET("/items/:id", controllers.GetItem)
+	route.PATCH("/items/:id", controllers.UpdateItem)
 
 	route.Run()
 }
